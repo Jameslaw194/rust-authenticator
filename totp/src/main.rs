cf1a6945417933520d6dd03410671012b8cc6e4f
Jameslaw194 <ketchupkingmc@gmail.com>
2025-09-04 16:07:15 +0100

    Moved QR code images to their own dedicated folder

--- totp/src/main.rs
+++ totp/src/main.rs
@@ -3,7 +3,7 @@ use image::ImageReader;
 use google_authenticator_converter;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = ImageReader::open("qr.png")?.decode()?.to_luma8();
+    let img = ImageReader::open("images/qr.png")?.decode()?.to_luma8();
 
     let mut img = rqrr::PreparedImage::prepare(img);
     let grids = img.detect_grids();
